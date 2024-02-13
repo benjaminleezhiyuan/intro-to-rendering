37d777f3f5cca1a5735d33c04a58c584aa00f638
benjamin <benjaminzhiyuan.lee@digipen.edu>
2024-02-13 15:46:45 +0800

    ass 5 done

--- Assignment 5 VPL/unittests_functions.h
+++ Assignment 5 VPL/unittests_functions.h
@@ -1,10 +1,10 @@
 /*!*****************************************************************************
 \file unittests_functions.h
 \author Vadim Surov (vsurov\@digipen.edu)
-\co-author YOUR NAME (SIT/DIGIPEN ID/EMAIL)
+\co-author Benjamin Lee (benjaminzhiyuan.lee\@digipen.edu)
 \par Course: CSD2151/CSD2150/CS250
 \par Assignment: 5.1 (Environment Mapping)
-\date 02/06/2024 (MM/DD/YYYY)
+\date 11/02/2024 (MM/DD/YYYY)
 \brief This file has declarations of functions for unit tests.
 
 This code is intended to be completed and submitted by a student,
